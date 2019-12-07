9b0d9f9d07cd5ad100c86e1171322cc324388351
Melissa Patterson <>
2019-12-07 10:44:11 -0600

    Handle /wildthings route :bear: :lion: :tiger:

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -1,6 +1,32 @@
 defmodule Servy.Handler do
   def handle(request) do
+    request
+    |> parse
+    |> route
+    |> format_response
+  end
 
+  def parse(request) do
+    [method, path, _] =
+      request
+      |> String.split("\n")
+      |> List.first
+      |> String.split(" ")
+    %{ method: method, path: path, resp_body: "" }
+  end
+
+  def route(conv) do
+    %{ conv | resp_body: "Bears, Lions, Tigers" }
+  end
+
+  def format_response(conv) do
+    """
+    HTTP/1.1 200 OK
+    Content-Type: text/html
+    Content-Length: #{byte_size(conv.resp_body)}
+
+    #{conv.resp_body}
+    """
   end
 end
 
@@ -12,7 +38,7 @@ Accept: */*
 
 """
 
-expected_response = """
+_expected_response = """
 HTTP/1.1 200 OK
 Content-Type: text/html
 Content-Length: 20
@@ -20,5 +46,5 @@ Content-Length: 20
 Bears, Lions, Tigers
 """
 
-response = Servy.Handler.handle(response)
+response = Servy.Handler.handle(request)
 IO.puts response
