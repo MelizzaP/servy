25da7d6410e03507e260e47eae7df9a6b573e436
Melissa Patterson <>
2019-12-08 09:53:27 -0600

    Use structure to define conv

--- lib/servy/plugins.ex
+++ lib/servy/plugins.ex
@@ -1,38 +1,42 @@
 require Logger
 
 defmodule Servy.Plugins do
-  def track(%{status: 404, path: path} = conv) do
+
+  alias Servy.Conv
+
+  def track(%Conv{status: 404, path: path} = conv) do
     Logger.warn "Warning: #{path} is on the loose!"
     conv
   end
 
-  def track(conv), do: conv
+  def track(%Conv{} = conv), do: conv
 
-  def rewrite_path(%{path: "/wildlife"} = conv) do
+  def rewrite_path(%Conv{path: "/wildlife"} = conv) do
     %{conv | path: "/wildthings"}
   end
 
-  def rewrite_path(%{path: path} = conv) do
+  def rewrite_path(%Conv{path: path} = conv) do
     regex = ~r{\/(?<thing>\w+)\?id=(?<id>\d+)}
     regex
     |> Regex.named_captures(path)
     |> rewrite_path_captures(conv)
   end
 
-  def rewrite_path_captures(%{"thing" => thing, "id" => id}, conv) do
+  def rewrite_path(%Conv{} = conv), do: conv
+
+  def rewrite_path_captures(%{"thing" => thing, "id" => id}, %Conv{} = conv) do
     %{ conv | path: "/#{thing}/#{id}" }
   end
 
-  def rewrite_path_captures(nil, conv), do: conv
+  def rewrite_path_captures(nil, %Conv{} = conv), do: conv
 
-  def rewrite_path(conv), do: conv
 
   @doc "adds emoji to success response"
-  def emojify(%{status: 200, resp_body: resp_body} = conv) do
+  def emojify(%Conv{status: 200, resp_body: resp_body} = conv) do
     emoji = String.duplicate("🤘", 3)
     body = emoji <> "\n" <> resp_body <> "\n" <> emoji
     %{ conv | resp_body: body }
   end
 
-  def emojify(conv), do: conv
+  def emojify(%Conv{} = conv), do: conv
 end
